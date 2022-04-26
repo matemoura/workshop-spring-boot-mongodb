4569e6f2dddd394df8f2567099f88536cc963f20
Mateus Moura <mateus51moura@gmail.com>
2022-04-26 00:14:55 -0300

    Getting a user by id

--- src/main/java/com/moura/course/services/UserService.java
+++ src/main/java/com/moura/course/services/UserService.java
@@ -1,20 +1,27 @@
 package com.moura.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.moura.course.domain.User;
 import com.moura.course.repository.UserRepository;
+import com.moura.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
